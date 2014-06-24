66eef589a53ba868e874e1bac490d5cbc3544fbc
Artem K <artem.karpov@yahoo.com>
2014-06-24 23:54:01 +0300

    Add 1. Simple test, wich not fails

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,18 @@
 from django.test import TestCase
+from apps.hello.models import Info
 
-# Create your tests here.
-class SomeTests(TestCase):
-    def test_math(self):
-        assert(2+2==5)
+class DataTest(TestCase):
+    def setUp(self):
+
+        Info.objects.create(info_firstname ="Buddy",
+                            info_lastname ="Budd",
+                            info_bio = "Hi there",
+                            info_email ="mail@mail.com",
+                            info_birth = "1967-05-06",
+                            info_jabber = "dont have",
+                            info_other = "nothing",
+                            info_skype= "nothing")
+    def test_1(self):
+        ''' will error on assertion'''
+        Buddy = Info.objects.get(info_firstname="Buddy")
+        self.assertEqual(Buddy.info_firstname, "Buddy")
