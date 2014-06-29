779667b63ad985d4d57a11344c1aca4fe7a1a588
Artem K <artem.karpov@yahoo.com>
2014-06-29 23:54:11 +0300

    Added Selenium test

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,5 +1,23 @@
 from django.test import TestCase
 from apps.hello.models import Info
+from selenium import webdriver
+import unittest
+
+class DataOutTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000/task/info/')
+        self.assertIn('InfoPage', self.browser.title)
+        self.fail('Finish the test!')
+
+
+
 
 class DataTest(TestCase):
     def setUp(self):
@@ -16,3 +34,7 @@ class DataTest(TestCase):
         ''' will error on assertion'''
         Buddy = Info.objects.get(info_firstname="Buddy")
         self.assertEqual(Buddy.info_firstname, "Budd")
+
+
+if __name__ == '__main__':  #7
+    unittest.main(warnings='ignore')
