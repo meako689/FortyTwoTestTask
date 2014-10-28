9005419e3feb1e5556eef0384ee618fcfc509627
artem_cheban <croozerua@gmail.com>
2014-10-28 10:57:06 +0200

    ticket:2 middleware schemamigration

---
+++ apps/hello/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Request'
+        db.create_table(u'hello_request', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('method', self.gf('django.db.models.fields.CharField')(max_length=10)),
+            ('path', self.gf('django.db.models.fields.TextField')()),
+            ('user', self.gf('django.db.models.fields.TextField')()),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'hello', ['Request'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Request'
+        db.delete_table(u'hello_request')
+
+
+    models = {
+        u'hello.request': {
+            'Meta': {'object_name': 'Request'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'path': ('django.db.models.fields.TextField', [], {}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.TextField', [], {})
+        }
+    }
+
+    complete_apps = ['hello']
