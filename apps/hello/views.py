547d0de6beec20939f2d9768f2b178514e610d8a
artem_cheban <croozerua@gmail.com>
2014-10-31 10:32:40 +0200

    ticket:3 #12 tabs issue

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -9,14 +9,14 @@ def home(request):
 def requests(request):
 	requests = Request.objects.all()
 	paginator = Paginator(requests, 25) # Show 25 contacts per page
-    page = request.GET.get('page')
-    try:
-        contacts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        contacts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        contacts = paginator.page(paginator.num_pages)
+	page = request.GET.get('page')
+	try:
+		contacts = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		contacts = paginator.page(1)
+	except EmptyPage:
+	# If page is out of range (e.g. 9999), deliver last page of results.
+	contacts = paginator.page(paginator.num_pages)
 
-	return render(request, 'requests.html', locals())
+return render(request, 'requests.html', locals())
