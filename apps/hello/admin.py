b6be83b0a3553616361fee7ee8e8717fbdd9c5bd
artem_cheban <croozerua@gmail.com>
2014-11-01 13:03:30 +0200

    ticket:1 added test passed

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from hello.models import Request
+# from hello.models import Request
 from django.db import models
 # Register your models here.
 
-class RequestAdmin(admin.ModelAdmin):
-	list_display = ['method', 'path', 'user', 'created_at', 'updated_at']
+# class RequestAdmin(admin.ModelAdmin):
+# 	list_display = ['method', 'path', 'user', 'created_at', 'updated_at']
 
-admin.site.register(Request, RequestAdmin)
+# admin.site.register(Request, RequestAdmin)
