a7e40285abe9cb64930cd06ee21200c89a5f30cd
Andriy Khavryuchenko <akhavr@khavr.com>
2015-03-20 21:04:30 +0200

    Fixed most flake8 warnings

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
 class SomeTests(TestCase):
     def test_math(self):
-        assert(2+2==5)
+        "put docstrings in your tests"
+        assert(2 + 2 == 5)
