63e9400e4d38378e9e034cb26cedcdcb893a0ed7
Elbtalkessel <netwintercom@gmail.com>
2020-07-01 22:44:15 +0200

    bump django

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -5,5 +5,7 @@ from django.test import TestCase
 
 class SomeTests(TestCase):
     def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+        """
+        put docstrings in your tests
+        """
+        self.assertEqual(2 + 2, 5)
